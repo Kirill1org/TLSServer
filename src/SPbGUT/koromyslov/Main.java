1b08dcad1b12446cb96012989bd40ac58fbb1577
Kirill1org <39536848+Kirill1org@users.noreply.github.com>
2020-01-15 08:08:42 +0300

    init

--- src/SPbGUT/koromyslov/Main.java
+++ src/SPbGUT/koromyslov/Main.java
@@ -1,21 +1,13 @@
 package SPbGUT.koromyslov;
 
-import java.io.IOException;
-import java.security.*;
-import java.security.cert.CertificateException;
-
 public class Main {
 
 
-    public static void main(String[] args) throws CertificateException, NoSuchAlgorithmException, KeyStoreException, IOException, UnrecoverableKeyException, KeyManagementException {
+    public static void main(String[] args) {
 
         TLSServer tlsServer = new TLSServer();
 
-        tlsServer.loadTrustStoreCert();
-        tlsServer.initTrustManagerFactory();
-        tlsServer.loadServerCert();
-        tlsServer.initKeyManagerFactory();
-        tlsServer.initSSLSocket();
+        tlsServer.init();
     }
 
 }
--- src/SPbGUT/koromyslov/TLSServer.java
+++ src/SPbGUT/koromyslov/TLSServer.java
@@ -1,13 +1,13 @@
 package SPbGUT.koromyslov;
 
-import sun.rmi.runtime.Log;
-
 import javax.net.ssl.*;
+import java.io.DataInputStream;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.*;
 import java.security.cert.CertificateException;
+import java.util.Arrays;
 
 public class TLSServer {
 
@@ -15,45 +15,91 @@ public class TLSServer {
     private KeyStore keyStore;
     private TrustManagerFactory trustManagerFactory;
     private KeyManagerFactory keyManagerFactory;
-    private SSLServerSocket sslServerSocket;
     private SSLContext sslContext;
 
-    public void loadTrustStoreCert() throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
-        trustStore = KeyStore.getInstance("BKS");
-        trustStore.load(new FileInputStream("C:/1/Key/keystore.bks"), "1234qwer!@#$QWER".toCharArray());
-        System.out.println(("LOADED CERT IS:" + trustStore.size()));
+    private byte[] recivedData;
+
+    private static int PORT=9090;
+
+    private void loadTrustStoreCert() {
+        try (FileInputStream fileInputStream = new FileInputStream("C:/1/key/1/Server/truststore.jks")) {
+            trustStore = KeyStore.getInstance("JKS");
+            trustStore.load(fileInputStream, "123qwe!@#QWE".toCharArray());
+        } catch (CertificateException | NoSuchAlgorithmException | KeyStoreException | IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("LoadTrustCert error");
+        }
 
     }
 
-    public void initTrustManagerFactory() throws NoSuchAlgorithmException, KeyStoreException {
+    private void initTrustManagerFactory() throws NoSuchAlgorithmException, KeyStoreException {
         trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
         trustManagerFactory.init(trustStore);
     }
 
 
-    public void loadServerCert() throws KeyStoreException, IOException, CertificateException, NoSuchAlgorithmException {
-        keyStore = KeyStore.getInstance("BKS");
-        keyStore.load(new FileInputStream("C:/1/Key/truststore,bks"), "123qwe!@#QWE".toCharArray());
+    private void loadKeyStoreCert() {
+        try (FileInputStream fileInputStream = new FileInputStream("C:/1/key/1/Server/keystore.jks")) {
+            keyStore = KeyStore.getInstance("JKS");
+            keyStore.load(fileInputStream, "123qwe!@#QWE".toCharArray());
+        } catch (CertificateException | NoSuchAlgorithmException | KeyStoreException | IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("LoadServerCert error");
+        }
     }
 
-    public void initKeyManagerFactory() throws NoSuchAlgorithmException, UnrecoverableKeyException, KeyStoreException {
+    private void initKeyManagerFactory() throws NoSuchAlgorithmException, UnrecoverableKeyException, KeyStoreException {
 
         keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-        keyManagerFactory.init(keyStore, "12345678".toCharArray());
+        keyManagerFactory.init(keyStore, "123qwe!@#QWE".toCharArray());
 
     }
 
-    public void initSSLSocket() throws NoSuchAlgorithmException, IOException, KeyManagementException {
+    private void initSSLSocket() {
 
-        sslContext = SSLContext.getInstance("TLS");
-        sslContext.init(keyManagerFactory.getKeyManagers(),trustManagerFactory.getTrustManagers(),null);
+        try {
+            sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            e.printStackTrace();
+            throw new RuntimeException("LoadServerCert error");
+        }
 
-        sslServerSocket=(SSLServerSocket) sslContext.getServerSocketFactory().createServerSocket(9090);
-        sslServerSocket.setNeedClientAuth(true);
+        try (SSLServerSocket sslServerSocket = (SSLServerSocket) sslContext.getServerSocketFactory().createServerSocket(PORT);
+             SSLSocket sslSocket = (SSLSocket) sslServerSocket.accept();
+             InputStream inputStream = sslSocket.getInputStream();
+             DataInputStream dataInputStream = new DataInputStream(inputStream)) {
 
-        //System.out.println("Server is successfully created!"+" "+sslServerSocket.getSSLParameters());
+            sslServerSocket.setNeedClientAuth(true);
+
+
+            int length = dataInputStream.readInt();
+            if (length != 0) {
+                recivedData = new byte[length];
+                dataInputStream.readFully(recivedData, 0, length);
+            }
+            System.out.println(Arrays.toString(recivedData));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException("InitSSL socket error");
+        }
 
 
     }
 
+    public void init() {
+        try {
+            loadTrustStoreCert();
+            initTrustManagerFactory();
+            loadKeyStoreCert();
+            initKeyManagerFactory();
+            initSSLSocket();
+        } catch (KeyStoreException | NoSuchAlgorithmException
+                | UnrecoverableKeyException e) {
+            e.printStackTrace();
+            throw new RuntimeException("InitKeys error");
+        }
+
+    }
 }
